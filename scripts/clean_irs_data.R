f8b54a365bbb1ff6dba855d7040ade276ef3c1ab
Ashlynn Ivy Wimer <ashewimer@gmail.com>
2023-12-07 17:08:38 -0600

    added additional data, cleaning

--- scripts/clean_irs_data.R
+++ scripts/clean_irs_data.R
@@ -12,15 +12,18 @@ library(stringr)
 
 PREFIX = '../data/IRS/'
 
-YEARS = c('2008-2009', '2009-2010', 
-          '2010-2011', '2011-2012', 
-          '2012-2013', '2013-2014', 
-          '2014-2015', '2015-2016')
+YEARS = c(2007, 2008,
+          2009, 2010, 
+          2011, 2012, 
+          2013, 2014, 
+          2015, 2016)
 
 #### Inflows
 
 # Directories
-old_format_inflow <- c('inflow/countyinflow0809.csv',
+old_format_inflow <- c('inflow/countyinflow0607.csv',
+                       'inflow/countyinflow0708.csv',
+                       'inflow/countyinflow0809.csv',
                        'inflow/countyinflow0910.csv',
                        'inflow/countyinflow1011.csv')
 
@@ -35,18 +38,25 @@ new_format_inflow <- c('inflow/countyinflow1112.csv',
 old_format_inflows <- old_format_inflow |> 
   map(.f = ~str_c(PREFIX, .x)) |> # fix names
   map(.f = ~read.csv(.x) |> # format Hell
+        filter(str_c(State_Code_Origin, County_Code_Origin) != 
+                 str_c(State_Code_Dest, County_Code_Dest)) |>
         mutate(State_Code_Dest = str_pad(State_Code_Dest, 2, 'left', '0'),
                    County_Code_Dest = str_pad(County_Code_Dest, 3, 'left', '0'),
-                   d_fips = str_c(State_Code_Dest, County_Code_Dest)) |>
+                   d_fips = str_c(State_Code_Dest, County_Code_Dest),
+                   Return_Num = str_remove_all(Return_Num, ","),
+                   Exmpt_Num  = str_remove_all(Exmpt_Num, ","),
+                   Aggr_AGI   = str_remove_all(Aggr_AGI, ",")) |>
         select(d_fips, Return_Num, Exmpt_Num, Aggr_AGI) |>
         group_by(d_fips) |>
-        summarise(n_return_in = sum(Return_Num),
-                  n_exempt_in = sum(Exmpt_Num),
-                  agi = sum(Aggr_AGI)))
+        summarise(n_return_in = sum(as.numeric(Return_Num), na.rm=TRUE),
+                  n_exempt_in = sum(as.numeric(Exmpt_Num), na.rm=TRUE),
+                  agi = sum(as.numeric(Aggr_AGI), na.rm=TRUE)))
 
 new_format_inflows <- new_format_inflow |>
   map(.f = ~str_c(PREFIX, .x)) |>
   map(.f = ~read.csv(.x) |>
+        filter(str_c(y1_statefips, y1_countyfips) != 
+                 str_c(y2_statefips, y2_countyfips)) |>  
         mutate(y2_statefips = str_pad(y2_statefips, 2, 'left', '0'),
                y2_countyfips = str_pad(y2_countyfips, 3, 'left', '0'),
                d_fips = str_c(y2_statefips, y2_countyfips)) |>
@@ -67,7 +77,7 @@ old_format_inflows <- old_format_inflows |>
 new_format_inflows <- new_format_inflows |>
   map(.x = 1:length(new_format_inflows),
       .f = ~mutate(new_format_inflows[[.x]], 
-                   year= YEARS[[.x + 3]]
+                   year= YEARS[[.x + 5]]
                    )) |>
   reduce(.f = rbind)
 
@@ -77,7 +87,9 @@ inflows <- rbind(old_format_inflows, new_format_inflows)
 #### Outflows
 
 # Directories
-old_format_outflow <- c('outflow/countyoutflow0809.csv',
+old_format_outflow <- c('outflow/countyoutflow0607.csv',
+                        'outflow/countyoutflow0708.csv',
+                        'outflow/countyoutflow0809.csv',
                         'outflow/countyoutflow0910.csv',
                         'outflow/countyoutflow1011.csv')
 
@@ -86,23 +98,29 @@ new_format_outflow <- c('outflow/countyoutflow1112.csv',
                         'outflow/countyoutflow1314.csv',
                         'outflow/countyoutflow1415.csv',
                         'outflow/countyoutflow1516.csv')
-
 # Clean data tables
 old_format_outflows <- old_format_outflow |> 
   map(.f = ~str_c(PREFIX, .x)) |> # fix names
-  map(.f = ~read.csv(.x) |> # format Hell
+  map(.f = ~read.csv(.x) |> 
+        filter(str_c(State_Code_Origin, County_Code_Origin) != 
+                 str_c(State_Code_Dest, County_Code_Dest)) |>
         mutate(State_Code_Origin = str_pad(State_Code_Origin, 2, 'left', '0'),
                County_Code_Origin = str_pad(County_Code_Origin, 3, 'left', '0'),
-               o_fips = str_c(State_Code_Origin, County_Code_Origin)) |>
+               o_fips = str_c(State_Code_Origin, County_Code_Origin),
+               Return_Num = str_remove_all(Return_Num, ","),
+               Exmpt_Num  = str_remove_all(Exmpt_Num, ","),
+               Aggr_AGI   = str_remove_all(Aggr_AGI, ",")) |>
         select(o_fips, Return_Num, Exmpt_Num, Aggr_AGI) |>
         group_by(o_fips) |>
-        summarise(n_return_out = sum(Return_Num),
-                  n_exempt_out = sum(Exmpt_Num),
-                  agi = sum(Aggr_AGI)))
+        summarise(n_return_out = sum(as.numeric(Return_Num), na.rm=TRUE),
+                  n_exempt_out = sum(as.numeric(Exmpt_Num), na.rm=TRUE),
+                  agi = sum(as.numeric(Aggr_AGI), na.rm=TRUE)))
 
 new_format_outflows <- new_format_outflow |>
   map(.f = ~str_c(PREFIX, .x)) |>
   map(.f = ~read.csv(.x) |>
+        filter(str_c(y1_statefips, y1_countyfips) != 
+                 str_c(y2_statefips, y2_countyfips)) |>  
         mutate(y1_statefips = str_pad(y1_statefips, 2, 'left', '0'),
                y1_countyfips = str_pad(y1_countyfips, 3, 'left', '0'),
                o_fips = str_c(y1_statefips, y1_countyfips)) |>
@@ -122,7 +140,7 @@ old_format_outflows <- old_format_outflows |>
 new_format_outflows <- new_format_outflows |>
   map(.x = 1:length(new_format_outflows),
       .f = ~mutate(new_format_outflows[[.x]], 
-                   year= YEARS[[.x + 3]]
+                   year= YEARS[[.x + 5]]
       )) |>
   reduce(.f = rbind)
 
@@ -132,8 +150,8 @@ outflows <- rbind(new_format_outflows, old_format_outflows)
 
 #### Save
 
-write.csv(outflows, '../data/irs_outflows.csv')
-write.csv(inflows, '../data/irs_inflows.csv')
+write.csv(outflows, '../data/irs_outflows.csv', row.names=FALSE)
+write.csv(inflows, '../data/irs_inflows.csv', row.names=FALSE)
 
 # Also save geographic information
 sf::st_write(tigris::counties(state="Michigan"), '../data/shapes/mi_counties.shp')
